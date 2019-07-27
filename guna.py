5ed5bb6681c75fae74c6d7e09b70d1a42c4a317e
Manjuguna <52068092+Manjuguna@users.noreply.github.com>
2019-07-27 15:33:30 +0530

    Create guna.py

---
+++ guna.py
@@ -0,0 +1,11 @@
+cns1=int(input())
+lit1=list(map(int,input().split()))
+att=[1]*cns1
+for daa in range(cns1):
+    if(daa==0):
+        if(lit1[daa]>li1[daa+1]):
+            att[daa]=att[daa]+att[daa+1]
+    elif(daa>0):
+        if(lit1[daa]>lit1[dt-1]):
+            att[daa]=att[daa]+att[daa-1]
+print(sum(att))
