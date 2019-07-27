2c7df53373e06cc6ed33b7296f880c583c15d171
Manjuguna <52068092+Manjuguna@users.noreply.github.com>
2019-07-27 15:37:18 +0530

    Update guna.py

--- guna.py
+++ guna.py
@@ -3,7 +3,7 @@ lit1=list(map(int,input().split()))
 att=[1]*cns1
 for daa in range(cns1):
     if(daa==0):
-        if(lit1[daa]>li1[daa+1]):
+        if(lit1[daa]>lit1[daa+1]):
             att[daa]=att[daa]+att[daa+1]
     elif(daa>0):
         if(lit1[daa]>lit1[daa-1]):
