d164084cb541d978cedc3fce8f4a4e415712375f
Manjuguna <52068092+Manjuguna@users.noreply.github.com>
2019-07-27 15:36:08 +0530

    Update guna.py

--- guna.py
+++ guna.py
@@ -6,6 +6,6 @@ for daa in range(cns1):
         if(lit1[daa]>li1[daa+1]):
             att[daa]=att[daa]+att[daa+1]
     elif(daa>0):
-        if(lit1[daa]>lit1[dt-1]):
+        if(lit1[daa]>lit1[daa-1]):
             att[daa]=att[daa]+att[daa-1]
 print(sum(att))
